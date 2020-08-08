00126d8a79397e143171c4415b7fe541ccb29842
Donald <donald_langston@yahoo.com>
2020-08-08 16:01:39 -0400

    Setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# git commands
+
+git init - create a new git repo
+git status - View the changes to your project code
+git add - Add files to staging area
+git commit - Creates a new commit with files from staging area
+git log - View recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === "production";
+    const CSSExtract = new MiniCssExtractPlugin({filename: 'styles.css'});
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 }
