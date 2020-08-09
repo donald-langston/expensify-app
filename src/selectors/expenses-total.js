35be599c1ae26bd9df65203eef6dd16336720a18
Donald <donald_langston@yahoo.com>
2020-08-09 18:33:43 -0400

    Add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,13 @@
+import expenses from "./expenses";
+
+export default (expenses) => {
+  if(expenses.length === 0) {
+      return 0;
+  } else {
+      return expenses
+        .map((expense) => expense.amount)
+        .reduce((sum, value) => {
+            return sum + value;
+        }, 0);
+  }  
+};
